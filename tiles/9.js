c195d9ee050e7736d39a5646b9f20cadf0d4b91b
diamondburned <diamond@arikawa-hi.me>
2022-05-07 01:02:21 -0700

    a

--- tiles/9.js
+++ tiles/9.js
@@ -9,33 +9,69 @@ tile.ballStart.velocity = { x: 9.61, y: 0 };
 tile.ballEnd.position = { x: 500, y: 111.82 };
 tile.ballEnd.velocity = { x: 8, y: -5.39 };
 
+let sensor;
 let trampoline;
 let trampoline2;
 let multiple = 1;
+let dawarudo;
+
+let script = document.createElement("script");
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.3/howler.min.js";
+script.onload = () => {
+  dawarudo = new Howl({
+    src: [
+      "https://cors-anywhere.herokuapp.com/https://www.myinstants.com/media/sounds/-hd-stardust-crusaders-za-warudo-mp3cut.mp3",
+    ],
+  });
+};
+
+document.head.appendChild(script);
+
+let stop = false;
+let once = false;
 
 tile.setup = function () {
-  // Initialize the above square variable to a valid rectangle. setup is
-  // guaranteed to be called before onTick is.
-  // square = tile.createRectangle(50, 50, 20, 20);
+  sensor = tile.createButton(30, 318, 50, 8, () => {
+    if (once) return;
+    once = true;
+
+    Matter.Body.set(tile.ball.body, { isStatic: true });
+    stop = true;
+
+    (async function () {
+      dawarudo.play();
+
+      await sleep(3000);
+
+      stop = false;
+      Matter.Body.set(tile.ball.body, { isStatic: false });
+    })();
+  });
+
   trampoline = tile.createSpring(30, 318, 50, 8, 1.5, -10);
+
   trampoline2 = tile.createSpring(430, 450, 100, 8, -10, -14);
   trampoline2.angle = -45;
+
   tile.createLine(300, 0, 300, 325, 20);
   tile.createLine(192, 418, 258, 483, 5);
 };
 
 tile.onTick = function () {
+  if (stop) return;
+
   // Rotate by 1.5deg every tick. With 60 TPS, this means rotating 90deg a
   // second.
   // translate tile
-  this.matter.Body.translate(trampoline.body, {x: 7 * multiple, y: 0});
-  if (trampoline.position.x > 280){
+  this.matter.Body.translate(trampoline.body, { x: 7 * multiple, y: 0 });
+  this.matter.Body.translate(sensor.body, { x: 7 * multiple, y: 0 });
+  if (trampoline.position.x > 280) {
     multiple = -1;
   }
-  if (trampoline.position.x < 20){
+  if (trampoline.position.x < 20) {
     multiple = 1;
   }
-}
+};
 
 // This function will run once when the tile loads for the first time
 // tile.setup = function () {
